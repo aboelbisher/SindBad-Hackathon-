e1df3071c267f11a435c0c4840d0cbbfc230240a
aboelbisher <aboelbisher.176@gmail.com>
2015-04-06 18:36:27 +0300

    added new Stam Class

---
+++ sindbad/sindbad/Models/StamClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sindbad.Models
+{
+    public class StamClass
+    {
+        //tel7aso teze ya e5wan :D 
+    }
+}
