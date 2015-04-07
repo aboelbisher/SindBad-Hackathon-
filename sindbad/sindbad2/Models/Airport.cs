abee805b8cfe5a0a9e2b89c53e2e3492aa99f0df
ahmedrewq12363 <ahmedrewq12363@gmail.com>
2015-04-07 12:24:22 +0300

    Flight +Airport

---
+++ sindbad/sindbad2/Models/Airport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sindbad2.Models
+{
+    public class Airport
+    {
+        String IAta { get; set; }
+        String name { get; set; }
+        String cityIata { get; set; }
+        String cityName { get; set; }
+        double locationLat { get; set; }
+        double locationLan { get; set; }
+
+    }
+    public class Flight
+    {
+        String DeparteTime { get; set; }
+        String ArrivingTime { get; set; }
+        String departAirportName { get; set; }
+        String ArrivingAirportName { get; set; }
+        double price { get; set; }
+    }
+}
